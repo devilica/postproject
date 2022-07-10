40346a542b720ebb1a2b094a17d38c265a203886
Milica <milicagalicdev@gmail.com>
2022-07-10 10:05:22 +0200

    added postgres database configuration

--- config/database.php
+++ config/database.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('postgres://jawjroltcdkpen:efd2822ae8496d2b1a872a4ef21491ea617e63868db6d1b4123d26ec0b09d27d@ec2-34-233-115-14.compute-1.amazonaws.com:5432/dab3lqcaqhclgh');
+
+
 return [
 
     /*
@@ -65,12 +68,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
