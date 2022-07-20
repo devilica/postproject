758ddfdd067ad774efca51a586d4e36c4e44094a
Milica <milicagalicdev@gmail.com>
2022-07-20 17:54:53 +0200

    change for images

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,6 +56,14 @@ return [
             'throw' => false,
         ],
 
+        'google' => [
+            'driver' => 'google',
+            'clientId' => env('MAIN_GOOGLE_DRIVE_CLIENT_ID'),
+            'clientSecret' => env('MAIN_GOOGLE_DRIVE_CLIENT_SECRET'),
+            'refreshToken' => env('MAIN_GOOGLE_DRIVE_REFRESH_TOKEN'),
+            'folderId' => env('MAIN_GOOGLE_DRIVE_FOLDER_ID'),
+        ],
+
     ],
 
     /*
